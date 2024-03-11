023b24747489fa2b032b8792e62ca5aac437a0c1
Abramovich85 <abramovich.peter.vladimirovich@gmail.com>
2024-03-11 21:24:21 +0300

    change models.py --> creat product and creat file workdjango.txt

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    count = models.IntegerField(default=0)
+
