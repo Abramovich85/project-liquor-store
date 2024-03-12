fb54a096c5c097cf7b5848d23a9525a5eb52f71e
Abramovich85 <abramovich.peter.vladimirovich@gmail.com>
2024-03-12 22:01:48 +0300

    add carousel in main

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 def index_page(request: HttpRequest):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{})
