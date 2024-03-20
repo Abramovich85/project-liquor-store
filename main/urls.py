691af1382d12153e943c1e4f10ca8e70d8825213
Abramovich85 <abramovich.peter.vladimirovich@gmail.com>
2024-03-20 21:22:49 +0300

    work in class add pages products, product and form, button cart

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index_page, name='home')
+    path('', views.index_page, name='home'),
+    path('products/', views.products_view, name='products'),
+    path('product/<int:id>/', views.product_view, name='product'),
+    path(
+        'product/<int:id>/add/',
+        views.add_to_basket_view,
+        name='add_to_basket'
+    ),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,37 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
 
 from .models import Product
 
 def index_page(request: HttpRequest):
     products = Product.objects.filter(is_active=True)
     return HttpResponse( render(request, 'main.html', {'products': products}))
+
+def products_view(request: HttpRequest):
+    products = Product.objects.filter(is_active=True)
+    products = products.order_by('-count')
+
+    return HttpResponse(render(request, 'products.html', {
+        'products': products
+    }))
+
+
+def product_view(request: HttpRequest, id: int):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404('Товар не найден')
+
+    return HttpResponse(render(request, 'product.html', {
+        'product': product
+    }))
+
+def add_to_basket_view(request: HttpRequest, id: int):
+    request.session['basket'] = request.session.get('basket', []) + [
+        {
+            'product_id': id,
+            'quantity': 1
+        }
+    ]
+    print(request.session['basket'])
+    return redirect('products')
