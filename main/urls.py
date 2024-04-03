931df1e56fadefdd001a535ad98ea354773b4797
Abramovich85 <abramovich.peter.vladimirovich@gmail.com>
2024-04-03 19:43:01 +0300

    Rework registration and add order

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index_page, name='home'),
     path('basket/', views.basket_view, name='basket'),
+    path('basket/clear/', views.basket_clear_view, name='basket_clear'),
+    path('order/', views.order_view, name='order'),
     path('product/<int:id>/', views.product_view, name='product'),
     path('<slug:category_slug>/', views.index_page, name='products'),
     path('<slug:category_slug>/<int:page>/', views.index_page, name='products'),
     path('product/<slug:product_slug>/', views.product_view, name='product'),
     path('product/<int:id>/add/', views.add_to_basket_view, name='add_to_basket'),
-    path('basket/clear/', views.basket_clear_view, name='basket_clear'),
+
 ]
